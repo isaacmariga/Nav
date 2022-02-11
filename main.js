fb910030b0903d13aae0a84c35e1c7fa325cddc8
isaacmariga <isaac.wang'ombe@student.moringaschool.com>
2022-02-11 15:32:19 +0300

    Adding hide js

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-let ham = document.querySelector("ham")
+let ham = document.querySelector(".ham")
+let links = document.querySelector(".links")
+
+
+ham.addEventListener('click', event => {
+    links.style.display = "block"
+})
+ham.addEventListener('dbclick', event => {
+    links.style.display = "none"
+})
