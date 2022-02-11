220b0292d616817c1597b9e77a46382d8654a98e
isaacmariga <isaac.wang'ombe@student.moringaschool.com>
2022-02-11 15:50:19 +0300

    Adding toggle back

--- main.js
+++ main.js
@@ -1,10 +1,22 @@
 let ham = document.querySelector(".ham")
+let ham2 = document.querySelector(".ham2")
 let links = document.querySelector(".links")
 
+// if (links.style.display = "none") {}
+// ham.addEventListener('toggle', event => {
+//         links.style.display = "block"
+//     })
+// ham.addEventListener('dbclick', event => {
+//     links.style.display = "none"
+// })
 
 ham.addEventListener('click', event => {
     links.style.display = "block"
+    ham.style.display = "none"
+    ham2.style.display = "block"
 })
-ham.addEventListener('dbclick', event => {
+ham2.addEventListener('click', event => {
     links.style.display = "none"
+    ham.style.display = "block"
+    ham2.style.display = "none"
 })
